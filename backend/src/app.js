1c24e6f9026bac09376677730fd5adc437585bc3
Luis Lomeli <lro.lomeli@gmail.com>
2026-04-22 01:17:26 -0600

    configurando docker

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,9 +3,13 @@ const express = require('express');
 const limiter = require('./middleware/rateLimiter');
 const authRouter = require('./routes/auth');
 const apiRouter = require('./routes/api');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors({
+  origin: 'https://luisrlp.com'
+}));
 app.use(limiter);
 app.use(express.json());
 app.use(authRouter);
