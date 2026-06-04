f246c0f948a96f35d43ffe9888a9deda45de1478
Luis Lomeli <lro.lomeli@gmail.com>
2026-06-04 14:16:18 -0600

    make CV app public (no login required)

--- frontend/apps/cv/main.jsx
+++ frontend/apps/cv/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import RequireAuth from '../../src/requireAuth.jsx'
 import './cv.css'
 import CvApp from './CvApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RequireAuth><CvApp /></RequireAuth>
+    <CvApp />
   </StrictMode>,
 )
